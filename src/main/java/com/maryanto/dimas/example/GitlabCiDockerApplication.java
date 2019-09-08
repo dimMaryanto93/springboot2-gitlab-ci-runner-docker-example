b341d8e775e1f780dc6da7b0a53b6065b6aef10a
dimasm93 <software.dimas_m@icloud.com>
2019-09-08 09:13:03 +0700

    update configuration

---
+++ src/main/java/com/maryanto/dimas/example/GitlabCiDockerApplication.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabCiDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabCiDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maryanto/dimas/example/GitlabCiDockerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maryanto.dimas.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitlabCiDockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
